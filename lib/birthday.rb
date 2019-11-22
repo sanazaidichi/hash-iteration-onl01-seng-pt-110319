4aad3962c7a7bd3e7cb2bfe1aead4a9f30426032
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-11-22 19:22:00 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,19 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
+
+def age_appropriate_birthday(happy_birthday)
+   happy_birthday.each do |kids_name, age|
+ if age == :age <12 && name 
+puts "Happy Birthday #{kids_name}You are now #{age} years old!""
+  end
+end 
+
+
+end 
 
 
